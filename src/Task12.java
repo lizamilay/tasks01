0e8ec3d7dcf831a035e05ab40c86f9f4126109e3
lizamilay <77553886+lizamilay@users.noreply.github.com>
2021-01-17 03:32:58 +0300

    Update Task12.java

--- src/Task12.java
+++ src/Task12.java
@@ -1,4 +1,4 @@
-
+//12. Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера (Таблицу ASCII).
 public class Task12 {
 	public static void main(String[] args) {
 		int a = 1;
