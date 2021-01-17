5d592c0c6c80f2379ac35706f05199e81de346ed
lizamilay <77553886+lizamilay@users.noreply.github.com>
2021-01-17 03:32:09 +0300

    Update Task10.java

--- src/Task10.java
+++ src/Task10.java
@@ -1,4 +1,4 @@
-
+//10. С помощью оператора while напишите программу вывода всех четных чисел в диапазоне от 2 до 100 включительно.
 public class Task10 {
 	public static void main(String[] args) {
 		int a;  
